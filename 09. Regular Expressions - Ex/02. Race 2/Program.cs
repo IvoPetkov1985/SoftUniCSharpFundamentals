419fa712ccb849101060ce2179a08dedb1bbdff7
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-05-09 17:57:08 +0300

    Tasks from the exercise

---
+++ 09. Regular Expressions - Ex/02. Race 2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._Race_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string patternNames = @"[A-Za-z]+";
+            string patternDigits = @"\d";
+
+            Dictionary<string, int> runners = new Dictionary<string, int>();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "end of race")
+            {
+                StringBuilder currentName = new StringBuilder();
+                int currentKm = 0;
+
+                MatchCollection letterMatches = Regex.Matches(inputLine, patternNames);
+                foreach (Match letter in letterMatches)
+                {
+                    currentName.Append(letter.Value);
+                }
+
+                MatchCollection digits = Regex.Matches(inputLine, patternDigits);
+                foreach (Match digit in digits)
+                {
+                    currentKm += int.Parse(digit.Value);
+                }
+
+                string newName = currentName.ToString();
+
+                if (names.Contains(newName) && !runners.ContainsKey(newName))
+                {
+                    runners.Add(newName, currentKm);
+                }
+                else if (runners.ContainsKey(newName))
+                {
+                    runners[newName] += currentKm;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            int place = 0;
+
+            foreach (var runner in runners.OrderByDescending(x => x.Value))
+            {
+                place++;
+
+                if (place > 3)
+                {
+                    break;
+                }
+
+                if (place == 1)
+                {
+                    Console.WriteLine($"1st place: {runner.Key}");
+                }
+                else if (place == 2)
+                {
+                    Console.WriteLine($"2nd place: {runner.Key}");
+                }
+                else if (place == 3)
+                {
+                    Console.WriteLine($"3rd place: {runner.Key}");
+                }
+            }
+        }
+    }
+}
---
+++ 09. Regular Expressions - Ex/03. SoftUni Bar Income 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._SoftUni_Bar_Income_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            string pattern = @"^%(?<custName>[A-Z][a-z]+)%[^\|\$\%\.]*?\<(?<product>\w+)\>[^\|\$\%\.]*?\|(?<count>\d+)\|[^\|\$\%\.]*?(?<price>\d+(\.\d+)?)\$$";
+
+            decimal income = 0;
+
+            while (inputLine != "end of shift")
+            {
+                Match match = Regex.Match(inputLine, pattern);
+
+                if (match.Success)
+                {
+                    string customer = match.Groups["custName"].Value;
+                    string product = match.Groups["product"].Value;
+                    int count = int.Parse(match.Groups["count"].Value);
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+
+                    decimal custSum = count * price;
+                    income += custSum;
+
+                    Console.WriteLine($"{customer}: {product} - {custSum:F2}");
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total income: {income:F2}");
+        }
+    }
+}

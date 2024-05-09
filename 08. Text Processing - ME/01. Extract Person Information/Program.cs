6d099828acb434a661d68c3afa52f7fe9735b926
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-05-09 15:39:05 +0300

    Two tasks from the More Exercise

---
+++ 08. Text Processing - ME/01. Extract Person Information/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                string inputLine = Console.ReadLine();
+                int firstNameIndex = inputLine.IndexOf('@') + 1;
+                int lastNameIndex = inputLine.IndexOf('|');
+                int firstAgeIndex = inputLine.IndexOf('#') + 1;
+                int lastAgeIndex = inputLine.IndexOf('*');
+
+                string name = inputLine[firstNameIndex..lastNameIndex];
+                string age = inputLine[firstAgeIndex..lastAgeIndex];
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
---
+++ 08. Text Processing - ME/02. Ascii Sumator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            string inputLine = Console.ReadLine();
+
+            StringBuilder resultLine = new StringBuilder();
+
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                if (firstChar < secondChar && inputLine[i] > firstChar && inputLine[i] < secondChar)
+                {
+                    resultLine.Append(inputLine[i]);
+                }
+                else if (firstChar > secondChar && inputLine[i] < firstChar && inputLine[i] > secondChar)
+                {
+                    resultLine.Append(inputLine[i]);
+                }
+            }
+
+            int sum = 0;
+
+            foreach (char symbol in resultLine.ToString())
+            {
+                sum += symbol;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
